c8d5408bac5d2219b32ddd2d918be80d0e974ea8
nikhil5467 <nikhilrocks374@gmail.com>
2016-09-14 00:27:17 +0530

    session5assignment4

---
+++ app3.js
@@ -0,0 +1,78 @@
+var employee1 ={
+    
+    name : "Nikhil Kumar",
+    age : 25,
+    salary : 30000,
+    address :{
+        
+        city : "Bengaluru",
+        state : "Karnataka",
+        pincode : 564100
+    }
+}
+var employee2 ={
+    
+    name : "Prakhar Agarwaal",
+    age : 26,
+    salary : 40000,
+    address :{
+        
+        city : "Bengaluru",
+        state : "Karnataka",
+        pincode : 564160
+    }
+}
+var employee3 ={
+    
+    name : "Aakash Kumar",
+    age : 25,
+    salary : 20000,
+    address :{
+        
+        city : "Bengaluru",
+        state : "Karnataka",
+        pincode : 554100
+    }
+}
+var employee4 ={
+    
+    name : "Piyush Kumar",
+    age : 25,
+    salary : 30000,
+    address :{
+        
+        city : "Bengaluru",
+        state : "Karnataka",
+        pincode : 565100
+    }
+}
+var employee5 ={
+    
+    name : "Rajiv Ranjan",
+    age : 25,
+    salary : 35000,
+    address :{
+        
+        city : "Bengaluru",
+        state : "Karnataka",
+        pincode : 564100
+    }
+}
+ 
+  var employeeArray = [];
+employeeArray.push(employee1);
+employeeArray.push(employee2);
+employeeArray.push(employee3);
+employeeArray.push(employee4);
+employeeArray.push(employee5);
+
+console.log(employeeArray[2].name);
+console.log(employeeArray[2].age);
+console.log(employeeArray[2].salary);
+console.log(employeeArray[2].address.city);
+console.log(employeeArray[2].address.state);
+console.log(employeeArray[2].address.pincode);
+
+
+     
+    
